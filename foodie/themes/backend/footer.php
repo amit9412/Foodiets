def8eec91e7c76d55ddfce4dd8b199a57df66d70
Amit Kumar Batham <34340900+amit9412@users.noreply.github.com>
2024-04-20 18:18:55 +0530

    Delete foodie directory

--- foodie/themes/backend/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <script>document.write(new Date().getFullYear())</script> © foodieits.
-            </div>
-        </div>
-    </div>
-</footer>
