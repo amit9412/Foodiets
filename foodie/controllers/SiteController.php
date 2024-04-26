2502db631185fcaa94c611f871c70c344b11416f
Sabire Rasul <itsabirerasul@gmail.com>
2024-04-27 00:53:06 +0530

    add login to frontend

--- foodie/controllers/SiteController.php
+++ foodie/controllers/SiteController.php
@@ -72,21 +72,21 @@ class SiteController extends Controller
      */
     public function actionLogin()
     {
-        // if (!Yii::$app->user->isGuest) {
-        //     return $this->goHome();
-        // }
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
 
-        // $model = new LoginForm();
-        // if ($model->load(Yii::$app->request->post()) && $model->login()) {
-        //     return $this->goBack();
-        // }
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
 
         // $model->password = '';
-        // return $this->render('login', [
-        //     'model' => $model,
-        // ]);
+        return $this->render('login', [
+            'model' => $model,
+        ]);
 
-        return $this->render('login');
+        // return $this->render('login');
     }
 
     /**
--- foodie/views/site/login.php
+++ foodie/views/site/login.php
@@ -1,6 +1,8 @@
 <?php
 
 use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 /** @var yii\web\View $this */
 
@@ -15,7 +17,7 @@ $this->title = 'Login';
             <h1>Login</h1>
             <nav>
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="<?= Url::to(['site/login']) ?>">Home</a></li>
+                    <li class="breadcrumb-item"><a href="<?= Url::to(['site/index']) ?>">Home</a></li>
                     <li class="breadcrumb-item active">Login</li>
                 </ol>
             </nav>
@@ -27,23 +29,28 @@ $this->title = 'Login';
         <div class="row justify-content-center">
             <div class="col-lg-4 col-md-6 col-sm-8">
                 <h2>Welcome Back !</h2>
-                <form>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Enter Mobile Number">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Enter Password">
-                    </div>
-                    <div class="form-group text-right">
-                        <a href="<?= Url::to(['site/forgot-password']) ?>" class="forgot-txt">Forgot Password?</a>
-                    </div>
-                    <div class="form-group">
-                        <a href="<?= Url::to(['site/user']) ?>" class="btn btn-block btn-primary">Login</a>
-                    </div>
-                    <p class="rgstr-txt text-center">Don't have an account?
-                        <a href="<?= Url::to(['site/signup']) ?>">Register</a>
-                    </p>
-                </form>
+
+                <?php $form = ActiveForm::begin(['action' => '', 'options' => ['class' => 'user form-group']]); ?>
+
+                <div class="form-group mb-3">
+                    <?= $form->field($model, 'username')->textInput(['maxlength' => 255, 'class' => 'form-control form-control-user', 'placeholder' => 'Enter Mobile Number'])->label(false) ?>
+                </div>
+
+                <div class="form-group mb-3">
+                    <?= $form->field($model, 'password')->passwordInput(['maxlength' => 255, 'class' => 'form-control form-control-user', 'placeholder' => 'Enter Password'])->label(false) ?>
+                </div>
+
+                <div class="form-group text-right">
+                    <a href="<?= Url::to(['site/forgot-password']) ?>" class="forgot-txt">Forgot Password?</a>
+                </div>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-success waves-effect waves-light w-100']) ?>
+                </div>
+                <p class="rgstr-txt text-center">Don't have an account?
+                    <a href="<?= Url::to(['site/signup']) ?>">Register</a>
+                </p>
+                <?php ActiveForm::end(); ?>
             </div>
         </div>
     </div>
